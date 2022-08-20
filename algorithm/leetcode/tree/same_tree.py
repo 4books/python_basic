fd9e7a4a0aab5d937327d0678f22e2a8dbe90de9
4books <joonwoo9192@gmail.com>
2022-08-20 09:19:09 +0900

    알고리즘

--- algorithm/leetcode/tree/same_tree.py
+++ algorithm/leetcode/tree/same_tree.py
@@ -9,6 +9,26 @@ class TreeNode:
         self.right = right
 
 
-class Soulution:
+class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        return True
+        return self.checkNode(p, q)
+
+    def checkNode(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if p is None and q is None:
+            # 둘다 None 인 경우
+            return True
+        elif p is None and q is not None:
+            # 한쪽만 None 인 경우
+            return False
+        elif p is not None and q is None:
+            return False
+        elif p.val == q.val:
+            # 둘다 값이 같은 경우
+            leftSame = self.checkNode(p.left, q.left)
+            rightSame = self.checkNode(p.right, q.right)
+            if leftSame and rightSame:
+                return True
+            return False
+        elif p.val != q.val:
+            # 값이 다른 경우
+            return False
